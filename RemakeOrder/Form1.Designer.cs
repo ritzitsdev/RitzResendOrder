9cfe27281ef3e0511fd847183da39faa90703c7f
ritzitsdev <jcampbell@ritzcamera.com>
2014-07-29 01:02:23 -0400

    Search and display Order

---
+++ RemakeOrder/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace RemakeOrder
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.lblOrderNum = new System.Windows.Forms.Label();
+      this.txtOrderNum = new System.Windows.Forms.TextBox();
+      this.btnSearch = new System.Windows.Forms.Button();
+      this.lblOrderInfo = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // lblOrderNum
+      // 
+      this.lblOrderNum.AutoSize = true;
+      this.lblOrderNum.Location = new System.Drawing.Point(3, 13);
+      this.lblOrderNum.Name = "lblOrderNum";
+      this.lblOrderNum.Size = new System.Drawing.Size(119, 13);
+      this.lblOrderNum.TabIndex = 0;
+      this.lblOrderNum.Text = "Enter an Order Number:";
+      // 
+      // txtOrderNum
+      // 
+      this.txtOrderNum.Location = new System.Drawing.Point(122, 7);
+      this.txtOrderNum.Name = "txtOrderNum";
+      this.txtOrderNum.Size = new System.Drawing.Size(100, 20);
+      this.txtOrderNum.TabIndex = 1;
+      // 
+      // btnSearch
+      // 
+      this.btnSearch.Location = new System.Drawing.Point(229, 5);
+      this.btnSearch.Name = "btnSearch";
+      this.btnSearch.Size = new System.Drawing.Size(75, 23);
+      this.btnSearch.TabIndex = 2;
+      this.btnSearch.Text = "Search";
+      this.btnSearch.UseVisualStyleBackColor = true;
+      this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+      // 
+      // lblOrderInfo
+      // 
+      this.lblOrderInfo.AutoSize = true;
+      this.lblOrderInfo.Location = new System.Drawing.Point(12, 51);
+      this.lblOrderInfo.Name = "lblOrderInfo";
+      this.lblOrderInfo.Size = new System.Drawing.Size(0, 13);
+      this.lblOrderInfo.TabIndex = 3;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(6, 68);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "label1";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(6, 88);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(35, 13);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "label2";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(312, 269);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.lblOrderInfo);
+      this.Controls.Add(this.btnSearch);
+      this.Controls.Add(this.txtOrderNum);
+      this.Controls.Add(this.lblOrderNum);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lblOrderNum;
+    private System.Windows.Forms.TextBox txtOrderNum;
+    private System.Windows.Forms.Button btnSearch;
+    private System.Windows.Forms.Label lblOrderInfo;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+  }
+}
+
---
+++ RemakeOrder/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RemakeOrder
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnSearch_Click(object sender, EventArgs e)
+    {
+      if (txtOrderNum.Text.Equals(String.Empty))
+      {
+        MessageBox.Show("Please enter an order number.");
+        return;
+      }
+
+      string orderNum = txtOrderNum.Text;
+      string orderFolder = orderNum + ".order";
+      string orderPath = findOrder(orderFolder);
+
+      if (orderPath.Equals(string.Empty))
+      {
+        lblOrderInfo.Text = "Order not found.";
+      }
+      else
+      {
+        showOrderInfo(orderPath, orderNum);
+        showProductInfo(orderPath, orderNum);
+      }
+    } //end btnSearch_Click
+
+    private void showOrderInfo(string orderPath, string orderNum)
+    {
+      string orderXmlPath = Path.Combine(orderPath, orderNum + ".xml");
+      XDocument orderXml = XDocument.Load(orderXmlPath);
+      var qryOrderFields = from orderFields in orderXml.Descendants("apm_order")
+                           select orderFields;
+      foreach (var orderField in qryOrderFields)
+      {
+        if (orderField.Element("store") != null)
+        {
+          addTxtBox("Store Number: ", orderField.Element("store").Attribute("store_id").Value, "txtStoreNum", 6, 68);
+          addTxtBox("Store Phone: ", orderField.Element("store").Attribute("store_phone").Value, "txtStorePhone", 6, 88);
+          addTxtBox("APM ID: ", orderField.Attribute("apm_id").Value, "txtApmId", 6, 108);
+          addTxtBox("Customer Name: ",
+            orderField.Element("shipment").Attribute("fname").Value + " " + orderField.Element("shipment").Attribute("lname").Value,
+            "txtCustName", 6, 128);
+          addTxtBox("Customer Phone: ", orderField.Element("shipment").Attribute("phone").Value, "txtCustPhone", 6, 148);
+        }
+        else
+        {
+          addTxtBox("Customer Name: ",
+            orderField.Element("shipment").Attribute("fname").Value + " " + orderField.Element("shipment").Attribute("lname").Value,
+            "txtCustName", 6, 68);
+          addTxtBox("Phone: ", orderField.Element("shipment").Attribute("phone").Value, "txtCustPhone", 6, 88);
+          addTxtBox("Email: ", orderField.Element("shipment").Attribute("email").Value, "txtCustEmail", 6, 108);
+          addTxtBox("Address: ", orderField.Element("shipment").Attribute("address1").Value, "txtCustAddr", 6, 128);
+          addTxtBox("City: ", orderField.Element("shipment").Attribute("city").Value, "txtCustCity", 6, 148);
+          addTxtBox("State: ", orderField.Element("shipment").Attribute("state").Value, "txtCustState", 6, 168);
+          addTxtBox("Zip: ", orderField.Element("shipment").Attribute("zip").Value, "txtCustZip", 6, 188);
+          addTxtBox("Delivery Method: ", orderField.Element("shipment").Attribute("delivery_method").Value, "txtDeliveryMethod", 6, 208);
+        }
+      }
+    } //end showOrderInfo
+
+    private void showProductInfo(string orderPath, string orderNum)
+    {
+      string orderXmlPath = Path.Combine(orderPath, orderNum + ".xml");
+      XDocument orderXml = XDocument.Load(orderXmlPath);
+      var qryProducts = from products in orderXml.Elements("apm_order").Elements("shipment").Elements("order_item")
+                             select products;
+      int i = 0;
+      double q = 12.4;
+      foreach (var product in qryProducts)
+      {
+        double dblyStart = (i + q) * 20;
+        int yStart = Convert.ToInt32(dblyStart);
+        Label redoLabel = new Label();
+        redoLabel.Text = "Remake this order item? ";
+        redoLabel.Left = 6;
+        redoLabel.Top = yStart;
+        redoLabel.Name = "redoLabel_" + i;
+
+        CheckBox chkBox = new CheckBox();
+        chkBox.Checked = false;
+        chkBox.Name = "chkRedo_" + i;
+        chkBox.Left = 24;
+        chkBox.Top = yStart;
+
+        Controls.Add(redoLabel);
+        Controls.Add(chkBox);
+
+        yStart += 20;
+        addTxtBox("Product: ", product.Attribute("name").Value, "txtProdName_" + i, 6, yStart);
+        yStart += 20;
+        addTxtBox("Product Id: ", product.Attribute("product").Value, "txtProductId_" + i, 6, yStart);
+        yStart += 20;
+        addTxtBox("Quantity: ", product.Attribute("quantity").Value, "txtQuantity_" + i, 6, yStart);
+        yStart += 20;
+
+        q = yStart / 20;
+        i += 1;
+      }
+    } //end showProductInfo
+
+    private void addTxtBox(string txtBoxLabel, string txtBoxValue, string txtBoxName, int xStart, int yStart)
+    {
+      Label txtLabel = new Label();
+      txtLabel.Text = txtBoxLabel;
+      txtLabel.Left = xStart;
+      txtLabel.Top = yStart;
+
+      TextBox txtBox = new TextBox();
+      txtBox.Name = txtBoxName;
+      txtBox.Text = txtBoxValue;
+      txtBox.Left = txtBoxLabel.Length + 6;
+      txtBox.Top = yStart;
+
+      Controls.Add(txtLabel);
+      Controls.Add(txtBox);
+    } //end addTxtBox
+
+    private static string findOrder(string orderFolder)
+    {
+      string orderPath = string.Empty;
+      string xmlSettings = @"C:\Program Files\ITS\RemakeOrder\remakeorder.xml";
+      XDocument folderPaths = XDocument.Load(xmlSettings);
+      var qrySearchFolders = from paths in folderPaths.Elements("folder_paths").Elements("search_folders").Elements("location")
+                           select paths;
+      foreach (XElement path in qrySearchFolders)
+      {
+        string searchFolder = Path.Combine(path.Value, orderFolder);
+        if (Directory.Exists(searchFolder))
+        {
+          orderPath = searchFolder;
+          break;
+        }
+      }
+      return orderPath;
+    } //end findOrder
+  }
+}
